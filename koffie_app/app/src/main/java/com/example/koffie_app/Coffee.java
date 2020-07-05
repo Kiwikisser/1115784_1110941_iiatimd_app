ed73c4a6b0f38ba9806ddd179b64d5ec580c0c86
Amos Peperkamp <amos.peperkamp@gmail.com>
2020-07-05 21:35:56 +0200

    added getters to coffee class, added dummy drawable, implemented viewholder functions

--- koffie_app/app/src/main/java/com/example/koffie_app/Coffee.java
+++ koffie_app/app/src/main/java/com/example/koffie_app/Coffee.java
@@ -1,13 +1,28 @@
 package com.example.koffie_app;
 
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+
 class Coffee {
     private String name;
     private String description;
-//    private String image;
+//    private String image = "dezeinfovoorbedankt.jpg";
+//    int resID;
 
     public Coffee(String name, String description){
         this.name = name;
         this.description = description;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+//    public String getImage() {
+//        return image;
+//    }
 }
--- koffie_app/app/src/main/java/com/example/koffie_app/CoffeeOverviewAdapter.java
+++ koffie_app/app/src/main/java/com/example/koffie_app/CoffeeOverviewAdapter.java
@@ -1,9 +1,13 @@
 package com.example.koffie_app;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 class CoffeeOverviewAdapter extends RecyclerView.Adapter<CoffeeOverviewAdapter.CoffeeOverviewViewHolder> {
@@ -23,9 +27,28 @@ class CoffeeOverviewAdapter extends RecyclerView.Adapter<CoffeeOverviewAdapter.C
         public CoffeeOverviewViewHolder(View v){
             super(v);
             textView = v.findViewById(R.id.coffeecard_textView);
-            imageView = v.findViewById(R.id.imageView);
+            imageView = v.findViewById(R.id.coffeecard_imageView);
         }
-//        setImageResource(R.drawable.my_image);
+    }
+
+    @NonNull
+    @Override
+    public CoffeeOverviewViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.coffee_overview_card,parent,
+                false);
+        CoffeeOverviewViewHolder coffeeOverviewViewHolder = new CoffeeOverviewViewHolder(v);
+        return coffeeOverviewViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CoffeeOverviewViewHolder holder, int position) {
+        holder.textView.setText(coffees[position].getName());
+        holder.imageView.setImageResource(R.drawable.dezeinfovoorbedankt);      // TODO: 05/07/2020 retrieve images from room db 
+    }
+
+    @Override
+    public int getItemCount() {
+        return coffees.length;
     }
 
 }
