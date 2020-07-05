45ebad923f70c89f5b9aa9f5a0c59f9f6e36720f
mauri985 <s1110941@student.hsleiden.nl>
2020-07-05 15:35:20 +0200

    Test Commit | Paar layouts voor de introductie

---
+++ koffie_app/app/src/main/java/com/example/koffie_app/CoffeeIntroductionActivity.java
@@ -0,0 +1,14 @@
+package com.example.koffie_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CoffeeIntroductionActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coffee_introduction);
+    }
+}
+
