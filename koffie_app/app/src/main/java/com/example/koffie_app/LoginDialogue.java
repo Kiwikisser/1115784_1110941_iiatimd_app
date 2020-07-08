58876a74cc5ea37618ed6ed728c1ae63282a8658
Amos Peperkamp <amos.peperkamp@gmail.com>
2020-07-08 23:41:37 +0200

    added base code and layouts for login modal/dialog

---
+++ koffie_app/app/src/main/java/com/example/koffie_app/LoginDialogue.java
@@ -0,0 +1,60 @@
+package com.example.koffie_app;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class LoginDialogue extends AppCompatDialogFragment {
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+
+//    @Override
+//    public void onCreate(Bundle savedInstancesState){
+//        super.onCreate(savedInstancesState);
+//        setStyle(AppCompatDialogFragment.STYLE_NO_TITLE, R.style.MyDialog);
+//    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        Window window = getDialog().getWindow();
+        window.setBackgroundDrawableResource(android.R.color.transparent);
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.layout_login, null);
+
+        builder.setView(view).setTitle("Login")
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        editTextUsername = view.findViewById(R.id.edit_username);
+        editTextPassword = view.findViewById(R.id.edit_password);
+
+        return builder.create();
+    }
+}
