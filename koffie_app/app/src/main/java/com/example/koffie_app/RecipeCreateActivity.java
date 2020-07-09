f26d6f822b5058c0a199f2e6f92da4a4f7c80117
mauri985 <s1110941@student.hsleiden.nl>
2020-07-09 13:13:40 +0200

    made it possible to store recipes when no internet connection is available, still need implementation to post offline data to live database

--- koffie_app/app/src/main/java/com/example/koffie_app/RecipeCreateActivity.java
+++ koffie_app/app/src/main/java/com/example/koffie_app/RecipeCreateActivity.java
@@ -26,25 +26,29 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.UUID;
+
 public class RecipeCreateActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
-    private final String POSTURL = "http://192.168.178.115:8000/api/recipes/create"; // path to store recipes
-    private String coffeebean; // variable for dropdown spinner
+    private final String POSTURL = "http://192.168.178.115:8000/api/recipes/create"; // path to store recipes, evt laravel online gooien ergens?
+    private String coffeebean; // variable for dropdown spinner value in form
     private UserRecipesViewModel userRecipesViewModel;
     AppRoomDatabase db;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.l_recipe_create);
-        db = AppRoomDatabase.getInstance(getApplicationContext());
-        Button submitRecipeButton = findViewById(R.id.button_submit_recipe);
-        submitRecipeButton.setOnClickListener(this);
-        userRecipesViewModel = ViewModelProviders.of(this).get(UserRecipesViewModel.class);
 
         Spinner spinner = findViewById(R.id.input_coffeeBean);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.coffeeTypes, android.R.layout.simple_spinner_item); //evt get coffetypes array from room coffee data
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+
+        db = AppRoomDatabase.getInstance(getApplicationContext());
+        userRecipesViewModel = ViewModelProviders.of(this).get(UserRecipesViewModel.class);
+
+        Button submitRecipeButton = findViewById(R.id.button_submit_recipe);
+        submitRecipeButton.setOnClickListener(this);
     }
 
     // Spinner Dropdown Methods
@@ -57,7 +61,7 @@ public class RecipeCreateActivity extends AppCompatActivity implements View.OnCl
     @Override
     public void onNothingSelected(AdapterView<?> parent) {}
 
-    // Get recipe data from form and check if any of them are empty, returns object with field data only if all fields aren't empty
+    // Gets recipe data from form. Returns object with inputfield data only if all fields aren't empty
     private JSONObject getRecipeFormData(){
         EditText recipeNameField = findViewById(R.id.input_recipeName);
         EditText recipeIngredientField = findViewById(R.id.input_ingredients);
@@ -80,13 +84,14 @@ public class RecipeCreateActivity extends AppCompatActivity implements View.OnCl
             recipePrepTimeField.setError( "Value required!");
             return null;
         }
-
+        String currentUsername = "tito"; // example, evt username getten && default username if user is not logged in
         String recipeName = recipeNameField.getText().toString();
         String recipeIngredients = recipeIngredientField.getText().toString();
         Integer recipeServings = Integer.parseInt(recipeServingsField.getText().toString());
         String recipePrepTime = recipePrepTimeField.getText().toString();
         JSONObject recipeObject = new JSONObject();
         try{
+            recipeObject.put("user_name", currentUsername);
             recipeObject.put("recipe_name", recipeName);
             recipeObject.put("recipe_ingredients", recipeIngredients);
             recipeObject.put("coffee_bean", coffeebean);
@@ -101,14 +106,13 @@ public class RecipeCreateActivity extends AppCompatActivity implements View.OnCl
 
     @Override
     public void onClick(View v) {
-        JSONObject newRecipeData = getRecipeFormData();
+        final JSONObject newRecipeData = getRecipeFormData();
         if(newRecipeData != null){
             RequestQueue queue = VolleySingleton.getInstance(this.getApplicationContext()).getRequestQueue();
             JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, POSTURL, newRecipeData, new Response.Listener<JSONObject>() {
                 @Override
                 public void onResponse(JSONObject response) {
-                    //start db, evt set related code into one function
-
+                    // connection, get response from laravel (+ generated id) add make&add an user recipe with the response data
                     try {
                         String recipe_id = response.getString("recipe_id");
                         String username = response.getString("user_name");
@@ -128,13 +132,20 @@ public class RecipeCreateActivity extends AppCompatActivity implements View.OnCl
             }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
-                    Log.d("error", error.getMessage());
+                    try {
+                        // no connection, locally add recipe to room
+                        String generated_recipe_id = UUID.randomUUID().toString().substring(0,9); // generate string id length 8 with a - at the end
+                        UserRecipes userRecipes = new UserRecipes(generated_recipe_id, "mauriccio", newRecipeData.getString("recipe_name"), newRecipeData.getString("recipe_ingredients"),
+                        newRecipeData.getString("coffee_bean"), newRecipeData.getInt("coffee_servings"), newRecipeData.getInt("coffee_prep_time"));
+                        new Thread(new InsertNewUserRecipe(db,userRecipes)).start();
+                        userRecipesViewModel.insert(userRecipes);
+                        // as soon as internet connection is made, send this offline recipe thats stored in room to the live database.  id doesnt need to be generated by laravel if this is the case.
+                        // an efficient way to get offline data ONLY is by checking if the room recipe records id ends with a "-" character
+                    } catch (JSONException e) {}
                 }
             });
-
             VolleySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
             userRecipesViewModel.getAllRecipes();
-            // evt redirect to recipe overview or success page
             Intent toIntroductionScreen = new Intent(this, UserRecipesOverviewActivity.class);
             startActivity(toIntroductionScreen);
         }
